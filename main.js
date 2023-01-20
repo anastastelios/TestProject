79683784fea8a7cf512666c0c9f6eb7fa1d962ed
anastastelios <anastas.stelios@gmail.com>
2023-01-20 23:53:13 +0100

    Created the index and the gallery page

--- main.js
+++ main.js
@@ -3,4 +3,26 @@ const navLinks = document.getElementsByClassName("nav__links")[0];
 
 toggleBtn.addEventListener("click", () => {
   navLinks.classList.toggle("active");
-})
+});
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
