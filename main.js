19a2ebae1455754b2f6945c245ba46b895bac3a7
anastastelios <anastas.stelios@gmail.com>
2023-01-21 21:09:12 +0100

    minor css changes

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const toggleBtn = document.getElementsByClassName("toggle-button")[0];
-const navLinks = document.getElementsByClassName("nav__links")[0];
+const navLinks = document.getElementsByClassName("nav-links")[0];
 
 toggleBtn.addEventListener("click", () => {
   navLinks.classList.toggle("active");
