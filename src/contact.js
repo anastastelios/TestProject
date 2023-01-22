0e0ef3521b20f110bf471366d861580f46e8aae5
anastastelios <anastas.stelios@gmail.com>
2023-01-22 05:08:10 +0100

    Added the Countries States and nationalities

--- src/contact.js
+++ src/contact.js
@@ -1,10 +1,14 @@
 import countries from '../data/countriesStates.js';
+import data from '../data/nationalities.json' assert {type:'json'};
 
+
+
+//////////////////////////////// Load countries dropdown //////////////////////////
 const populateSelect = () => {
 
   const countriesList = Object.keys(countries);
 
-  let countySelect = document.getElementById('select-country');
+  const countySelect = document.getElementById('select-country');
   countriesList.filter((country) => {
     return country !== null;
   }).forEach((country) => {
@@ -14,6 +18,10 @@ const populateSelect = () => {
 
 window.onload = populateSelect();
 
+
+
+
+//////////////////////////// Load States of Selected Country ////////////////////////
 $('#select-state').prop("disabled", true);
 $('#select-country').on('change', function(e) {
   $('#select-state').empty();
@@ -23,7 +31,15 @@ $('#select-country').on('change', function(e) {
   (countryId > -1 && currentStates.length > 0) ? $('#select-state').prop("disabled", false) : $('#select-state').prop("disabled", true);
   (currentStates.length > 0) && currentStates.forEach((state) => {
     const stateOption = $("<option>").text(state);
-      $('#select-state').append(stateOption);
+    $('#select-state').append(stateOption);
   });
   (currentStates.length === 0) && $('#select-state').append($("<option>").text('Skip this...'));
 });
+
+
+//////////////////////////// Load Nationalities Dropdown /////////////////////////////
+
+data.forEach((country) => {
+  const nationalityOption = $("<option>").text(country.nationality);
+    $('#select-nationality').append(nationalityOption);
+})
