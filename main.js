de79d4676cc5773748f796aec04a7ff27df099b5
anastastelios <anastas.stelios@gmail.com>
2023-01-23 06:09:46 +0100

    minor change

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.esm.browser.min.js';
+
 //////////////////////////////////// HAMBURGER MENU //////////////////////////////////
 
 const toggleBtn = document.getElementsByClassName('toggle-button')[0];
@@ -5,4 +7,28 @@ const navLinks = document.getElementsByClassName('nav-links')[0];
 
 toggleBtn.addEventListener('click', () => {
   navLinks.classList.toggle('active');
+});
+
+///////////////////////////////// Slider Swiper /////////////////////////////////////
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
 });
